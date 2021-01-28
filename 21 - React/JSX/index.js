bc94ee13bc60b52880bf43746cad47254b7928c4
RENEAS <rodney.martins@live.com>
2021-01-27 21:08:13 -0300

    21 - React Basico

---
+++ 21 - React/JSX/index.js
@@ -0,0 +1,9 @@
+const root = document.querySelector('#root')
+const page = ()=>{
+return  <div>
+            <h1 class="Titulo">Hello</h1>
+            <h4 class="SUB Titulo">Hello</h4>
+        </div>
+}
+
+ReactDOM.render(page, root)
---
+++ 21 - React/base/index.js
@@ -0,0 +1,5 @@
+const root = document.querySelector('#root')
+const t1 = React.createElement("h1", {id:"Titulo Principal"},"Hello World")
+const t2 = React.createElement("h4", {id:"Titulo Principal"},"Hello World")
+const headline = React.createElement("div", null, t1, t2)
+ReactDOM.render(headline, root)
